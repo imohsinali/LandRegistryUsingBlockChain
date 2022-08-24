8612402a48a1b51f4ea179007b5e2251aaf89bbc
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-24 15:23:34 +0500

    Create landDetailList.js

---
+++ FrontEnd/src/components/Dasboard/landDetailList.js
@@ -0,0 +1,78 @@
+import land1 from "../../images/land1.jpg";
+import land2 from "../../images/land2.jpg";
+import land3 from "../../images/falcon-9.webp";
+
+const landList = [
+  {
+    id: 0,
+    title: "Land - 1",
+    img: land1,
+    date: "on Aug 23",
+    price: "90 lac",
+  },
+  {
+    id: 1,
+    title: "Land - 2",
+    img: land2,
+    date: "on Aug 24",
+    price: "90 lac",
+  },
+  {
+    id: 2,
+    title: "Land - 3",
+    img: land3,
+    date: "on Aug 25",
+    price: "90 lac",
+  },
+  {
+    id: 3,
+    title: "Land - 4",
+    img: land1,
+    date: "on Aug 24",
+    price: "90 lac",
+  },
+  {
+    id: 4,
+    title: "Land - 5",
+    img: land3,
+    date: "on Aug 23",
+    price: "90 lac",
+  },
+  {
+    id: 5,
+    title: "Land - 6",
+    img: land2,
+    date: "on Aug 26",
+    price: "90 lac",
+  },
+  {
+    id: 6,
+    title: "Land - 7",
+    img: land3,
+    date: "on Aug 24",
+    price: "90 lac",
+  },
+  {
+    id: 7,
+    title: "Land - 8",
+    img: land1,
+    date: "on Aug 25",
+    price: "90 lac",
+  },
+  {
+    id: 8,
+    title: "Land - 9",
+    img: land2,
+    date: "on Aug 23",
+    price: "90 lac",
+  },
+  {
+    id: 9,
+    title: "Land - 10",
+    img: land3,
+    date: "on Aug 24",
+    price: "90 lac",
+  },
+];
+
+export default landList;
