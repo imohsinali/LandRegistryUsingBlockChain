96970bc8b0d285f6722a5e2029d35e8a32e512c4
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-25 11:58:03 +0500

    Update MenuList.js

--- FrontEnd/src/components/Dasboard/MenuList.js
+++ FrontEnd/src/components/Dasboard/MenuList.js
@@ -11,18 +11,21 @@ import PeopleIcon from "@mui/icons-material/People";
 import LogoutIcon from "@mui/icons-material/Logout";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import LandscapeIcon from "@mui/icons-material/Landscape";
+import { Link } from "react-router-dom";
 
 const list = [
   {
     id: 0,
     icon: <DashboardIcon />,
     text: "Dashboard",
+    path: "/",
   },
   {
     id: 1,
 
     icon: <AddPhotoAlternateIcon />,
     text: "Land Gallery",
+    path: "/landgallery",
   },
 
   {
@@ -30,18 +33,21 @@ const list = [
 
     icon: <LandscapeIcon />,
     text: "My Land",
+    path: "/",
   },
   {
     id: 3,
 
     icon: <PeopleIcon />,
     text: "profile",
+    path: "/profile",
   },
   {
     id: 4,
 
     icon: <LogoutIcon />,
     text: "logout",
+    path: "/",
   },
 ];
 
@@ -49,28 +55,36 @@ const Menu = ({ open }) => {
   return (
     <List component="nav">
       {list.map((item) => {
-        const { icon, text, id } = item;
+        const { icon, text, id, path } = item;
         return (
-          <ListItem key={id} disablePadding sx={{ display: "block" }}>
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-              }}
-            >
-              <ListItemIcon
+          <Link
+            to={path}
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+            }}
+          >
+            <ListItem key={id} disablePadding sx={{ display: "block" }}>
+              <ListItemButton
                 sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
                 }}
               >
-                {icon}
-              </ListItemIcon>
-              <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-          </ListItem>
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  {icon}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          </Link>
         );
       })}
 
