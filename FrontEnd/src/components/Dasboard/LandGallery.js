bfaef5c9140056612577816409fec92999a323e2
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-25 11:57:57 +0500

    Update LandGallery.js

--- FrontEnd/src/components/Dasboard/LandGallery.js
+++ FrontEnd/src/components/Dasboard/LandGallery.js
@@ -1,24 +1,50 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 import Title from "./Title";
+import landList from "./landDetailList";
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
-export default function LandGallery({ img, title, price, date }) {
+export default function LandGallery() {
   return (
-    <div className="Land-container">
-      <Title className="Land-title">{title}</Title>
+    <Container maxWidth="100%" sx={{ mt: 4, mb: 4 }}>
+      <Grid container spacing={3}>
+        {landList.map((item) => {
+          const { img, title, price, date, id } = item;
+          return (
+            <Grid item xs={12} md={4} lg={3} key={id}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  flexWrap: "Wrap",
+                  minHeight: "300px",
+                  overflow: "hidden",
+                }}
+              >
+                <div className="Land-container">
+                  <Title className="Land-title">{title}</Title>
 
-      <img src={img} alt="" className="myLand" />
+                  <img src={img} alt="" className="myLand" />
 
-      <div className="Land-details">
-        <Typography component="p" variant="h5">
-          Rs:{price}
-        </Typography>
-        <Typography color="text.secondary">{date}</Typography>
-      </div>
-    </div>
+                  <div className="Land-details">
+                    <Typography component="p" variant="h5">
+                      Rs:{price}
+                    </Typography>
+                    <Typography color="text.secondary">{date}</Typography>
+                  </div>
+                </div>
+              </Paper>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
   );
 }
