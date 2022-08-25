2fb4af609ca6b4988fa31a51bd9b795054e97718
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-25 11:57:42 +0500

    Update App.js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,9 +1,25 @@
 import Dashboard from "./components/Dasboard/Dashboard";
 import Registration from "./components/Registration";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandGallery from "./components/Dasboard/LandGallery";
 function App() {
   // return <Registration />;
-  return <Dashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/landgallery"
+          element={<Dashboard props={<LandGallery />} />}
+        />
+        <Route
+          path="/profile"
+          element={<Dashboard props={<Registration />} />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
