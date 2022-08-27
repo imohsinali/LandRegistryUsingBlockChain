fa6028277b8b75da5501ff418503958b9c1db337
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-27 19:47:32 +0500

    Update App.js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,25 +4,32 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dasboard/Dashboard";
 import LandGallery from "./components/Dasboard/LandGallery";
 import Layout from "./components/Dasboard/Layout";
+import { useState } from "react";
 function App() {
   // return <LandGallery />;
+  const [login, setLogin] = useState(false);
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/"
-          element={<Layout co={<Dashboard />} title={"DashBoard"} />}
-        />
-        <Route
-          path="/landgallery"
-          element={<Layout co={<LandGallery />} title={"LandGallery"} />}
-        />
-        <Route
-          path="/profile"
-          element={<Layout co={<Registration />} title={"Profile"} />}
-        />
-      </Routes>
+      {login ? (
+        <Routes>
+          <Route
+            path="/"
+            element={<Layout co={<Dashboard />} title={"DashBoard"} />}
+          />
+          <Route
+            path="/landgallery"
+            element={<Layout co={<LandGallery />} title={"LandGallery"} />}
+          />
+          <Route
+            path="/profile"
+            element={<Layout co={<Registration />} title={"Profile"} />}
+          />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/landRegistry/login" element={<Login />} />
+        </Routes>
+      )}
     </Router>
   );
 }
