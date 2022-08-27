3ae76161aa44a09b4a797035870c1235fed9d67b
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-27 13:06:40 +0500

    working on routing

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,22 +1,26 @@
-import Dashboard from "./components/Dasboard/Dashboard";
 import Registration from "./components/Registration";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dasboard/Dashboard";
 import LandGallery from "./components/Dasboard/LandGallery";
+import Layout from "./components/Dasboard/Layout";
 function App() {
-  // return <Registration />;
+  // return <LandGallery />;
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={<Layout co={<Dashboard />} title={"DashBoard"} />}
+        />
         <Route
           path="/landgallery"
-          element={<Dashboard props={<LandGallery />} />}
+          element={<Layout co={<LandGallery />} title={"LandGallery"} />}
         />
         <Route
           path="/profile"
-          element={<Dashboard props={<Registration />} />}
+          element={<Layout co={<Registration />} title={"Profile"} />}
         />
       </Routes>
     </Router>
