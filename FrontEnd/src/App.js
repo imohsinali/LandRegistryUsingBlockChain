126fbe6a95671bdf40570079d8482a4f86667ae9
imohsinali <mohsinbalty@gmail.com>
2022-09-08 18:19:38 +0500

    Update App.js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -5,31 +5,28 @@ import Dashboard from "./components/Dasboard/Dashboard";
 import LandGallery from "./components/Dasboard/LandGallery";
 import Layout from "./components/Dasboard/Layout";
 import { useState } from "react";
+import Protected from "./components/Protected";
 function App() {
   // return <LandGallery />;
   const [login, setLogin] = useState(false);
+  console.log(login);
+  console.log(localStorage.getItem("login"));
   return (
     <Router>
-      {login ? (
-        <Routes>
-          <Route
-            path="/"
-            element={<Layout co={<Dashboard />} title={"DashBoard"} />}
-          />
-          <Route
-            path="/landgallery"
-            element={<Layout co={<LandGallery />} title={"LandGallery"} />}
-          />
-          <Route
-            path="/profile"
-            element={<Layout co={<Registration />} title={"Profile"} />}
-          />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route path="/landRegistry/login" element={<Login />} />
-        </Routes>
-      )}
+      <Routes>
+        <Route path="login" element={<Login />} />
+
+        <Route path="/" element={<Protected Component={Dashboard} />} />
+        <Route
+          path="/profile"
+          element={<Protected Component={Registration} />}
+        />
+        <Route
+          path="/landgallery"
+          element={<Protected Component={LandGallery} />}
+        />
+        {/* <Route path="/logout" element={<Protected />} /> */}
+      </Routes>
     </Router>
   );
 }
