090c7acdf6eac02470524b0e2bff512f71bbaeb9
imohsinali <mohsinbalty@gmail.com>
2022-09-08 18:20:20 +0500

    Create Protected.js

---
+++ FrontEnd/src/components/Protected.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Layout from "./Dasboard/Layout";
+const Protected = ({ Component }) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    let login = localStorage.getItem("login");
+    if (!login) {
+      navigate("login");
+    }
+  });
+  return <Layout co={<Component />} />;
+};
+// function Login() {
+//   const navigate = useNavigate();
+//   const login = () => {
+//     localStorage.getItem("login", true);
+//     navigate("/");
+//   };
+//   React.useEffect(() => {
+//     let login = localStorage.getItem("login");
+//     if (login) {
+//       navigate("/");
+//     }
+//   });
+//   return (
+//     <>
+//       <h1>Login page</h1>
+//       <button onClick={login}>Login</button>
+//     </>
+//   );
+// }
+
+export default Protected;
