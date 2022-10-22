b492d54a7b5f51fd79b46768a7a8e19f4ba1e648
Mohsin Ali <59974279+imohsinali@users.noreply.github.com>
2022-10-22 14:47:44 +0500

    Create User.js

---
+++ FrontEnd/src/User/User.js
@@ -0,0 +1,33 @@
+import Registration from "./Registration";
+import Login from "./Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import LandGallery from "./LandGallery";
+import Protected from "./Protected";
+import Home from "../Home";
+function User() {
+  const login =localStorage.getItem("login")
+  // let login= false
+  
+  return (
+    <Router>
+      <Routes>
+       <Route  path="/" element={<Home />} />
+      
+     <Route path="login" element={<Login />} />
+      <Route path="dashboard" element={<Protected Component={Dashboard} />} />
+      <Route
+        path="reg"
+        element={<Protected Component={Registration} />}
+      />
+      <Route
+        path="landgallery"
+        element={<Protected Component={LandGallery} />}
+      />
+    </Routes>
+      
+    </Router>
+  );
+}
+
+export default User;
